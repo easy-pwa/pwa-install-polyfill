1769e564057caa36adc3aa840437e10b72763999
Adrien Peyre <dev@adrien.peyre.fr>
2026-03-07 16:07:20 +0100

    Switch to Node 24

--- bin/docker.sh
+++ bin/docker.sh
@@ -2,11 +2,12 @@
 
 DIRECTORY=$(dirname $(realpath $0 ))
 
-docker run -it --rm \
+TTY_FLAG=$([ -t 0 ] && echo "-it" || echo "-i")
+docker run $TTY_FLAG --rm \
   --name pwa-install-polyfill \
   -v "$DIRECTORY/..":/home/node/app \
   -w /home/node/app \
   -p 8080:8080 \
   -u $(id -u ${USER}):$(id -g ${USER}) \
-  node:20 \
-  bash
+  node:24-slim \
+  "${@:-bash}"
